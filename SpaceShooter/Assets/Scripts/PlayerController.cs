1b19352adc67fbcd12fec630522fe43a0dd35f36
hanzg <hanzg@hanzgs-MacBook-Pro.local>
2017-03-20 19:52:22 +0900

    Space Shooter Tutorial 1.5 finished

--- SpaceShooter/Assets/Scripts/PlayerController.cs
+++ SpaceShooter/Assets/Scripts/PlayerController.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable] //in order to view properties in Unity's inspector 
+public class Boundary{
+	public float xMin, xMax, zMin, zMax;
+}
+	
 public class PlayerController : MonoBehaviour {
 
-	private Rigidbody rb;
+	public Boundary boundary;
 	public float speed;
+	public float tilt;
+
+	private Rigidbody rb;
+
 //	public Text messageText;
 
 	void Start() {
@@ -16,7 +25,16 @@ public class PlayerController : MonoBehaviour {
 	void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-		rb.velocity = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed;
+
+		rb.position = new Vector3 (
+			Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * (-tilt));
+			
 	}
 }
 
