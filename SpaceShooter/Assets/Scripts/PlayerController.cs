6686d4da85b3c7e4c780fdbd813a19d1cd478cb3
hanzg <hanzg@hanzgs-MacBook-Pro.local>
2017-03-20 18:54:44 +0900

    Space Shooter Tutorial 1.4 finished

---
+++ SpaceShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+//	public Text messageText;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		rb.velocity = new Vector3(moveHorizontal, 0.0f, moveVertical);
+	}
+}
+
