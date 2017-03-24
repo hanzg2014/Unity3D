b23c4cd75fcbf148fca8ec0e71103f870fab5c27
hanzg <hanzg@hanzgs-MacBook-Pro.local>
2017-03-24 10:11:54 +0900

    Tutorial Interface&Essentials 3.1 finished

---
+++ Interface&Essentials/Assets/Editor/LevelScriptEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelScript))]
+public class LevelScriptEditor : Editor {
+
+	//method called everytime when a new inspector is drawn inside Unity
+	public override void OnInspectorGUI(){
+		LevelScript myLevelScript	= (LevelScript)target;
+		myLevelScript.experience = EditorGUILayout.IntField ("Experience", myLevelScript.experience);	//any change done need to be written back
+		EditorGUILayout.LabelField ("Level", myLevelScript.Level.ToString()); //read-only; label contronl requires a string value
+
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
