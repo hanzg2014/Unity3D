352252957c92e9ee7a4de520aacfd77c190e43b9
hanzg <hanzg@hanzgs-MacBook-Pro.local>
2017-03-25 09:22:34 +0900

    Interface&Essentials final

---
+++ Interface&Essentials/Assets/Editor/SomeScriptEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(SomeScript))]
+public class SomeScriptEditor : Editor 
+{
+	public override void OnInspectorGUI(){
+//		SomeScript mySomeScript	= (SomeScript)target;
+//
+//		EditorGUILayout.IntField ("Level", mySomeScript.level);
+//		EditorGUILayout.FloatField ("Health", mySomeScript.health);
+//		EditorGUILayout.Vector3Field ("Target", mySomeScript.target);
+
+
+		DrawDefaultInspector();	//will draw all the defaults, while still allowing customization
+		EditorGUILayout.HelpBox("This is a help box", MessageType.Info);
+
+
+	}
+}
